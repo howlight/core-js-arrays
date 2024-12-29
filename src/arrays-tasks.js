d0c0ccb24d111612bad154a9dbb8e88095554be9
Aleksey <howlight19@gmail.com>
2024-12-30 01:28:06 +0300

    feat: complete shiftArray task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -627,8 +627,13 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const arrayLength = arr.length;
+  let shift = n;
+  if (shift < 0) {
+    shift += arrayLength; // Если сдвиг отрицательный, преобразуем его в положительный эквивалент
+  }
+  return arr.slice(-shift).concat(arr.slice(0, arrayLength - shift));
 }
 
 /**
